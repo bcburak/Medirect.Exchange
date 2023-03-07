c44257263ff678c23535df07b72a9d62549953e4
Burak Celik <burak.celik@mentormate.com>
2023-03-07 21:13:08 +0300

    user hourly rate limit getting from user repo

--- MeDirect.Exchange.API/Middlewares/RateLimiterMiddleware.cs
+++ MeDirect.Exchange.API/Middlewares/RateLimiterMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using MeDirect.Exchange.Application.Constants;
+using MeDirect.Exchange.Application.Interfaces.Repositories;
 using MeDirect.Exchange.Application.Services.Interfaces;
 
 namespace MeDirect.Exchange.API.Middlewares
@@ -7,25 +8,27 @@ namespace MeDirect.Exchange.API.Middlewares
     {
         private readonly RequestDelegate _next;
         private readonly ICacheService _cacheService;
+        private readonly IUserRepository _userRepository;
 
-        public RateLimiterMiddleware(RequestDelegate next, ICacheService cacheService)
+        public RateLimiterMiddleware(RequestDelegate next, ICacheService cacheService, IUserRepository userRepository)
         {
             _next = next;
             _cacheService = cacheService;
+            _userRepository = userRepository;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             var requestedEndpoint = context.Request.Path.Value;
             var route = context.GetRouteData();
-            var userId = route.Values["userId"];
+            var userId = Convert.ToInt32(route.Values["userId"]);
             // Uncomment depends if needed ip with userId
             //var ipAddress = context.Connection.RemoteIpAddress.ToString();
 
 
             if (requestedEndpoint.Contains("executeCurrencyExchange"))
             {
-
+                var userHourlyRateLimit = _userRepository.GetByIdAsync(userId).Result.TradesPerHourLimit;
                 // Set the key for the client's currency exchange count
                 var key = $"{userId}-{CacheKeys.RateLimit}";
 
@@ -37,7 +40,7 @@ namespace MeDirect.Exchange.API.Middlewares
                 _cacheService.Add(key, count, TimeSpan.FromHours(1));
 
                 // If the client has exceeded the limit, return a 429 Too Many Requests response
-                if (count > 10)
+                if (count > userHourlyRateLimit)
                 {
                     context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
                     await context.Response.WriteAsync("Too many currency exchange requests.");
